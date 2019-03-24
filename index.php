39ab2c49f65a7331b374e979c1fb2abefb5f1ccc
vektorelss <cangulsedat1@gmail.com>
2019-03-24 12:22:35 +0200

    yorum yaptık.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
-
+//burası neresi?
 include('functions/main_functions.php');
-
+//orası burası
 include('inc/header.php');
 
 $mesaj_no = (isset($_GET['mesaj'])) ? $_GET['mesaj'] : 0;
@@ -10,7 +10,7 @@ if(!isset($_GET['main'])){
 	include('inc/main/main.php');
 } else {
 	include('inc/main/'.$_GET['main'].'.php');
-}
+} //orası neresi?
 
 include('inc/footer.php');
 
